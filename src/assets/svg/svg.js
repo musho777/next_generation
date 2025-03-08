e3aaac4d6c7b8605bd5840a2c9596d816be18360
musho777 <129147695+musho777@users.noreply.github.com>
2025-03-08 16:41:04 +0400

    navbar

---
+++ src/assets/svg/svg.js
@@ -0,0 +1,15 @@
+export const SignIn = () => {
+  return <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M16 18H7C5.89543 18 5 17.1046 5 16V12H7V16H16V2H7V6H5V2C5 0.89543 5.89543 0 7 0H16C17.1046 0 18 0.89543 18 2V16C18 17.1046 17.1046 18 16 18ZM9 13V10H0V8H9V5L14 9L9 13Z" fill="white" />
+  </svg>
+}
+
+export const MenuSvg = () => {
+  return <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7 3C5.34315 3 4 4.34315 4 6H20C20 4.34315 18.6569 3 17 3H7Z" fill="white" />
+    <path d="M7 11C5.34315 11 4 9.65685 4 8H20C20 9.65685 18.6569 11 17 11H7Z" fill="white" />
+    <path d="M7 13C5.34315 13 4 14.3431 4 16H20C20 14.3431 18.6569 13 17 13H7Z" fill="white" />
+    <path d="M7 21C5.34315 21 4 19.6569 4 18H20C20 19.6569 18.6569 21 17 21H7Z" fill="white" />
+  </svg>
+
+}
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import './styles.css'
+import myImage from '../../assets/images/logo.png';
+import { useLocation } from 'react-router-dom';
+import { MenuSvg, SignIn } from '../../assets/svg/svg';
+export const Navbar = () => {
+  const [language, setLanguage] = useState("en")
+  const location = useLocation();
+  return <div className="navbar">
+    <div>
+      <div className="logo">
+        <img src={myImage} alt="logo" />
+        <p>Next Generation <br /> Armenia</p>
+      </div>
+      <div className="navbar_links">
+        <a
+          href="/#Teachers"
+          className={location.hash === "#Teachers" ? "navbar_active_link" : ""}
+        >
+          Teachers
+        </a>
+        <a
+          href="/#Donors"
+          className={location.hash === "#Donors" ? "navbar_active_link" : ""}
+        >
+          Donors
+        </a>
+
+        <a
+          href="/#AboutUs"
+          className={location.hash === "#AboutUs" ? "navbar_active_link" : ""}
+        >
+          About us
+        </a>
+        <a
+          href="/#ContactUs"
+          className={location.hash === "#ContactUs" ? "navbar_active_link" : ""}
+        >
+          Contact us
+        </a>
+        <a
+          href="/#Blog"
+          className={location.hash === "#Blog" ? "navbar_active_link" : ""}
+        >
+          Blog
+        </a>
+      </div>
+      <div className="language">
+        <p onClick={() => setLanguage("en")} className={language == "en" && "activeLanguage"}>Eng</p>
+        <p onClick={() => setLanguage("am")} className={language == "am" && "activeLanguage"}>Հայ</p>
+        <p onClick={() => setLanguage("ru")} className={language == "ru" && "activeLanguage"}>Рус</p>
+      </div>
+      <div className="sign_In">
+        <SignIn />
+        <p>Sign In</p>
+      </div>
+      <div className="menu">
+        <MenuSvg />
+      </div>
+    </div>
+  </div>
+}
